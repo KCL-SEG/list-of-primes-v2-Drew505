f4025e70f5fd7c1f25ecf8260c0d31186575a292
Andrei Borodin <borodinandrew321@gmail.com>
2022-10-22 19:42:31 +0100

    Final version
    finished exercise

--- primes.py
+++ primes.py
@@ -2,5 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <=0:
+        raise ValueError('Number is 0 or negative.')
     list = []
+    i = 2
+    while len(list) < number_of_primes:
+        for j in range(2, i):
+            if i % j == 0:
+                break
+        else:
+            list.append(i)
+        i +=1
     return list
+
